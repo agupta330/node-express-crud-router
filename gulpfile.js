74752d9b35b9009bcb17415155c2f1b7ea1b8f99
Dennis Ahaus <dennis.ahaus@gmail.com>
2015-09-24 17:52:54 +0000

    Bugfix gulp-coverage is not available

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@
   'use strict';
   var gulp = require('gulp');
   var mocha = require('gulp-mocha');
-  var cover = require('gulp-coverage');
 
   gulp.task('test', function() {
 
@@ -10,16 +9,9 @@
       .src('test/suite.js', {
         read: false
       })
-      // .pipe(cover.instrument({
-      //   pattern: ['lib/**/*.js']
-      // }))
-      // gulp-mocha needs filepaths so you can't have any plugins before it
       .pipe(mocha({
         reporter: 'spec'
       }))
-      // .pipe(cover.gather())
-      // .pipe(cover.format())
-      // .pipe(gulp.dest('reports/coverage'));
   });
 
 }());
