f4ca8f8eddd152b8be4d355f07e058c41b1c5459
Dennis Ahaus <dennis.ahaus@gmail.com>
2015-09-28 14:02:21 +0000

    Finish issue2-bulk-update

--- test/tests/update.js
+++ test/tests/update.js
@@ -4,6 +4,7 @@
   var expect = require('expect');
   var fixtures = require("../fixtures.js");
   var supertest = require('supertest');
+  var us = require('underscore');
 
   var defaultModelData = fixtures.defaultModelData;
   var baseUrl = fixtures.baseUrl;
@@ -49,6 +50,85 @@
         .end(function(err, res) {
           done(err);
         });
+
+    });
+
+    it(' - should execute bulk update with returning numberAffected', function(done) {
+
+      var removeAll = supertest(fixtures.app)
+        .delete('/api/users')
+        .expect(200);
+
+      var data1 = us.clone(defaultModelData);
+      data1.counter = 1;
+      var add1 = supertest(fixtures.app)
+        .post('/api/users')
+        .expect(200)
+        .send(data1);
+
+      var data2 = us.clone(defaultModelData);
+      data2.counter = 2;
+      var add2 = supertest(fixtures.app)
+        .post('/api/users')
+        .expect(200)
+        .send(data2);
+
+      var data3 = us.clone(defaultModelData);
+      data3.counter = 2;
+      var add3 = supertest(fixtures.app)
+        .post('/api/users')
+        .expect(200)
+        .send(data3);
+
+      var getBeforeUpdate = supertest(fixtures.app)
+        .get('/api/users')
+        .expect(200)
+        .expect(function(res) {
+          expect(res.body.length).toBe(3);
+          expect(res.body[0].counter).toBe(1);
+          expect(res.body[1].counter).toBe(2);
+          expect(res.body[2].counter).toBe(2);
+        })
+
+      var criteria = {
+        counter: 2
+      }
+
+      var doUpdate = supertest(fixtures.app)
+        .put('/api/users?criteria=' + JSON.stringify(criteria))
+        .expect(200, {
+          numberAffected: 2
+        })
+        .send({
+          counter: 1234
+        });
+
+      var getAfterUpdate = supertest(fixtures.app)
+        .get('/api/users')
+        .expect(200)
+        .expect(function(res) {
+          expect(res.body.length).toBe(3);
+          expect(res.body[0].counter).toBe(1);
+          expect(res.body[1].counter).toBe(1234);
+          expect(res.body[2].counter).toBe(1234);
+        })
+
+      removeAll.end(function(err0) {
+        add1.end(function(err1) {
+          add2.end(function(err2) {
+            add3.end(function(err3) {
+              getBeforeUpdate.end(function(err4) {
+                doUpdate.end(function(err5) {
+                  getAfterUpdate.end(function(err6) {
+                    done(err0 || err1 || err2 || err3 || err4 || err5 || err6);
+                  })
+                })
+              })
+            })
+          })
+        })
+      })
+
 
     });
 
